6f897459f863716c258dc555c9cb87a5aaba10dd
ifthekar <sajalcse2018@gmail.com>
2023-05-11 17:34:04 +0600

    Perform All Arithmetic Operator

---
+++ performAlllArithmeticOperations.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+     int  num1,num2,sum,div,sub,mul,mod;
+     printf("Enter Two Numbers::");
+     scanf("%d %d",&num1,&num2);
+     sum=num1+num2;
+     sub=num1-num2;
+     mul=num1*num2;
+     div=num1/num2;
+     mod=num1%num2;
+
+     printf("SUM =%d\n",sum);
+     printf("SUB =%d\n",sub);
+     printf("MUL =%d\n",mul);
+     printf("DIV =%d\n",div);
+     printf("MOD =%d\n",mod);
+
+return 0;
+}
+
+
+
+
+
