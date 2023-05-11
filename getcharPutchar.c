6029125a39c33f553d8f2cad00cf381da9591403
ifthekar <sajalcse2018@gmail.com>
2023-05-11 12:28:14 +0600

    using getchar() and putchar() function

---
+++ getcharPutchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+      char grade;
+      printf("Enter Student Grade::");
+      //input character using getchar() & store grade
+      grade=getchar();
+      //output grade value
+      putchar(grade);
+
+return 0;
+}
