5a76375d54d79dcdbaef01f6e26c559f978736e9
ifthekar <sajalcse2018@gmail.com>
2023-05-11 14:59:20 +0600

    C program to demonstrate input output using scanf() and print()

---
+++ scanfPrintfFunction.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    int number1,number2,sum;
+    printf("Enter First Number=");
+    scanf("%d",&number1);
+    printf("Enter Second Number=");
+    scanf("%d",&number2);
+    sum=number1+number2;
+    printf("Sum is= %d",sum);
+
+return 0;
+}
